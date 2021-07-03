a6f46f073786098c88afb4de6d92396f762d3e7c
Luis Deutsch <LuisDeutsch98@gmail.com>
2021-07-03 20:36:05 +0200

    changed port

--- email/app.js
+++ email/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const nodemailer = require("nodemailer")
 const app = express()
-const port = 3000
+const port = 30100
 const sqlite3 = require('sqlite3').verbose();
 const axios = require('axios');
 
