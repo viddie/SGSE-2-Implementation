d2c123a1460f44642446e2d2e99bb662f96d2cc9
Marius Warning <MWarning.Minden@gmail.com>
2021-07-04 16:36:51 +0200

    added authorisation to mongodb

--- chat/server.js
+++ chat/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 const PORT = 8080;
 
-mongoose.connect('mongodb://172.17.0.6:27017/chat',  {useUnifiedTopology: true } , { useNewUrlParser: true } )
+mongoose.connect('mongodb://root:passwort123!@172.17.0.6:27017/chat',  {useUnifiedTopology: true } , { useNewUrlParser: true } )
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected succesfully'))
@@ -18,3 +18,6 @@ const config = require("./routes/configuration")
 app.use("/configuration", config)
 
 app.listen(PORT, () => console.log("Server Started"))
+
+// export app for testing
+module.exports = app;
