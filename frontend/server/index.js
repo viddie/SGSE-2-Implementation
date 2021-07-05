92c5a993d9d53ecd959c860924c4cbe817cf5d32
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 22:01:15 +0200

    HhqaCSFK

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express()
 const port = 3001
 
-app.use(express.static(__dirname + '/dist'), {index: '_'});
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port, () => {
   console.log(`Uuuuuuuunnnnnnnnd looooooosgehts ..... Frontend gestartet!`);
