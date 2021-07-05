6660084c2d2705555f387bfdb7a7640fc961be88
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 21:24:30 +0200

    Next

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.sendFile('public/index.html');
+});
+
+app.get('/App.js', function(req, res){
+  res.sendFile('public/App.js');
+}); 
 
 app.listen(port, () => {
   console.log(`Frontend gestartet!`);
