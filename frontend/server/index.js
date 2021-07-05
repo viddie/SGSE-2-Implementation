3d1602d817b3141efdceefd88aff9d17f6ef949c
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 22:34:07 +0200

    234tfgs

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -3,13 +3,8 @@ const path = require('path');
 const app = express()
 const port = 3001
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public', {index: false}))
 
-app.get('/', function(req, res){
-
-  // save html files in the `views` folder...
-  res.sendFile(__dirname + "/views/index.html");
-});
 // Start server
 app.listen(port, function () {
   console.log('Server up and running on ', `http://localhost:${port}/`);
