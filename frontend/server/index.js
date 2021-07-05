53f4ecf2ae29902694459b6dd6b4a4fd7eac08ab
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 21:54:19 +0200

    Pushy

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -3,12 +3,11 @@ const path = require('path');
 const app = express()
 const port = 3001
 
-app.use(express.static(__dirname + '/dist'));
-
-app.get('*', function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port, () => {
   console.log(`Frontend gestartet!`);
