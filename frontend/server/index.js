f88445e3865241c1149dd1fd6b40ac5236772c97
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 22:20:12 +0200

    Zuz

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -3,17 +3,13 @@ const path = require('path');
 const app = express()
 const port = 3001
 
-const STATIC = path.resolve(__dirname, 'dist');
-const INDEX = path.resolve(STATIC, 'index.html');
+app.use(express.static(__dirname + '/public'))
 
-// Static content
-app.use(express.static(STATIC));
+app.get('/', function(req, res){
 
-// All GET request handled by INDEX file
-app.get('*', function (req, res) {
-  res.sendFile(INDEX);
+  // save html files in the `views` folder...
+  sendfile(__dirname + "/views/index.html");
 });
-
 // Start server
 app.listen(port, function () {
   console.log('Server up and running on ', `http://localhost:${port}/`);
