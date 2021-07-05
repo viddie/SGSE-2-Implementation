46b272475adb7bda06ec092524fafb69f31fb5ba
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 21:57:22 +0200

    Pushchchc

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -3,12 +3,8 @@ const path = require('path');
 const app = express()
 const port = 3001
 
-app.get('/', function (request, response) {
-  response.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'), {index: '_'});
 
 app.listen(port, () => {
-  console.log(`Frontend gestartet!`);
+  console.log(`Uuuuuuuunnnnnnnnd looooooosgehts ..... Frontend gestartet!`);
 })
