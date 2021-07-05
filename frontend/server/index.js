fe0eb98cd363949028b54d57697d932fdaaff8bd
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 21:47:56 +0200

    Nextttt

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,18 +1,14 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3001
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname+'/index.html');
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.get('/App.js', function(req, res){
-  res.sendFile(__dirname+'/App.js');
-}); 
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Route not found" });
-})
 
 app.listen(port, () => {
   console.log(`Frontend gestartet!`);
