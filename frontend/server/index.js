d29b9cfec43c93f3125f1b6d9056daec36403d73
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 22:47:21 +0200

    fdgh

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express()
 const port = 3001
 
-app.use(express.static(__dirname + '/public', {index: false}))
+
 app.get('/', function (req, res) {
   console.log('###GET REQUEST received');
   res.sendFile(__dirname + '/public/App.js'); 
 });
+app.use(express.static(__dirname + '/public', {index: false}))
 // Start server
 app.listen(port, function () {
   console.log('Server up and running on the pod!');
