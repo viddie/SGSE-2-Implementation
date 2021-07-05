b28cb51da7818e8b40da3b31f90b412ae9fbfa64
tnentwig <tristan.nentwig@googlemail.com>
2021-07-05 22:38:27 +0200

    dfasg

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -4,7 +4,11 @@ const app = express()
 const port = 3001
 
 app.use(express.static(__dirname + '/public', {index: false}))
-
+app.get('/', function (req, res) {
+  console.log('###GET REQUEST received');
+  console.log(req);
+  res.sendFile(__dirname + '/public/index.html'); 
+});
 // Start server
 app.listen(port, function () {
   console.log('Server up and running on ', `http://localhost:${port}/`);
