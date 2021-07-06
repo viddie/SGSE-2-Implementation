036249905aab549212c6271ebd39c0132b1d149e
Marius Warning <MWarning.Minden@gmail.com>
2021-07-06 17:17:19 +0200

    rollback of auth until implemented fully

--- chat/routes/messages.js
+++ chat/routes/messages.js
@@ -8,7 +8,7 @@ const accessTokenSecret = 'somerandomaccesstoken';
 
 const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
-
+    next();/*
     if (authHeader) {
         const token = authHeader.split(' ')[1];
 
@@ -22,7 +22,7 @@ const authenticateJWT = (req, res, next) => {
         });
     } else {
         res.sendStatus(401);
-    }
+    }*/
 }
 
 // Getting all
