0e9bfd022709c592632d4006dcd3d5d3801d52d5
Marius Warning <MWarning.Minden@gmail.com>
2021-07-06 21:26:49 +0200

    or did I?

--- frontend/src/components/headerLoggedIn/index.js
+++ frontend/src/components/headerLoggedIn/index.js
@@ -21,7 +21,8 @@ const Header = (props) => {
                         <Nav.Link href="#features">Angebot erstellen</Nav.Link>
                         <a class="nav-link" id="chat-button" onClick={() => {
                             alert("This is a Cry for Help!");
-                            document.getElementsByClassName('App')[0].appendChild(Chat);
+                            const app = document.querySelector('div.App');
+                            app.appendChild(Chat)
                         }}>Chat</a>
                         <NavDropdown title="User" id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Profil</NavDropdown.Item>
