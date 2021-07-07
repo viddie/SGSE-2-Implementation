1ff30dfa23bf4362051096ff64c4b0c2921c9038
tnentwig <tristan.nentwig@googlemail.com>
2021-07-07 22:36:14 +0200

    PushItToTheFloor

--- frontend/src/header/header.jsx
+++ frontend/src/header/header.jsx
@@ -3,12 +3,6 @@ import { Button, Navbar, NavDropdown, Nav, Form, FormControl, Container, Image }
 import logo from '/static/wurm.png'
 
 
-/**
- * @author
- * @function Header
- **/
-
-
 const Header = (props) => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
@@ -16,20 +10,14 @@ const Header = (props) => {
             <Container>
                 <Navbar.Brand href="http://sgse2.ad.fh-bielefeld.de">
                     Barter 
-                    <Image src="./wurm.png" height="40"></Image>
+                    <Image src={logo} height="40"></Image>
                     Smarter
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Angebot erstellen</Nav.Link>
-                        <Nav.Link href="#pricing">Chat Test</Nav.Link>
-                        <NavDropdown title="User" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.2">Sign Up</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#pricing">Kontakt</Nav.Link>
+                        <Nav.Link href="#login">Login</Nav.Link>
+                        <Nav.Link href="#createAccount">Konto erstellen</Nav.Link>
                     </Nav>
                     <Nav>
                         <Form className="d-flex">
