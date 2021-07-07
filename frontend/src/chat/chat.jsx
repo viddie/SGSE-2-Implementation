341f50f57f742d0aae6fe8e7e0311b7750384b16
Marius Warning <MWarning.Minden@gmail.com>
2021-07-07 18:00:45 +0200

    implemented chat message fetch at start

--- frontend/src/chat/chat.jsx
+++ frontend/src/chat/chat.jsx
@@ -17,11 +17,13 @@ function Chat(props) {
                 <div>Options</div>
             </header>
             <section>
-                <ChatRoom receiver={props.receiver} token={props.token}/>
+                <ChatRoom receiver="Ben" token="Alf"/>
             </section>
         </div>
     );
 }
+
+// <ChatRoom receiver={props.receiver} token={props.token}/>
 
 function ChatRoom(props) {
     const receiver = props.receiver;
@@ -30,11 +32,10 @@ function ChatRoom(props) {
     
     const [formValue, setFormValue] = useState('');
 
+    const [messages] = getMessages(receiver, token);
 
     const sendMessage = async (e) => {
         e.preventDefault();
-
-        const { uid, photoURL } = { user, image };
 
         const requestOptions = {
             method: 'POST',
@@ -97,6 +98,58 @@ function ChatMessage(props) {
             </div>
         </>
     );
+}
+
+function getMessages(other_user, token) {
+    //var this_user = extract_username_from_token(token);
+    this_user = token;
+    var data = ApiCall(this_user, other_user).push(ApiCall(other_user, this_user));
+
+    console.log(data)
+
+    function compareTimestamps(a, b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        return (a<b)?-1:(a>b)?1:0;
+    }
+
+    data.sort( function(a, b) {
+        return compareTimestamps(a.timestamp, b.timestamp);
+    });
+
+    return data;
+}
+
+function ApiCall(user1, user2) {
+    const data = {}
+    const getMessages = async (e) => {
+        e.preventDefault();
+
+        const requestOptions = {
+            method: 'GET'
+        };
+        fetch(`http://sgse2.ad.fh-bielefeld.de/api/chat/receive/${user1}/${user2}`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                data = isJson && await response.json();
+    
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+            })
+            .catch(error => {
+                this.setState({ errorMessage: error.toString() });
+                console.error('Error while sending chat message: API call malfunctioned', error);
+            });
+
+        setFormValue('');
+        dummy.current.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    return data
 }
 
 export default Chat
