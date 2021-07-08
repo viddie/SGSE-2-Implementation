67bf3fb22be2e302e70687fb14a8487e5b7aa9a6
Marius Warning <MWarning.Minden@gmail.com>
2021-07-08 22:18:09 +0200

    Mama, just killed a Website. Put a commit into its Repo, pulled the Enter-Button now its dead

--- frontend/src/chat/chat.jsx
+++ frontend/src/chat/chat.jsx
@@ -128,6 +128,8 @@ function getMessages(other_user, token) {
 
     Promise.all([data1, data2]).then(function(val) {
         data = [...val[0],...val[1]];
+        console.log("DEBUG: getMessages: val")
+        console.log(val);
     });
 
     console.log("DEBUG: getMessages: data")
