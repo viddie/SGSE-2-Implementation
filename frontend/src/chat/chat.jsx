495786c5f27da00214f62419b7c044b61ef14ddd
Marius Warning <MWarning.Minden@gmail.com>
2021-07-08 15:09:15 +0200

    is this real life?

--- frontend/src/chat/chat.jsx
+++ frontend/src/chat/chat.jsx
@@ -41,6 +41,7 @@ function ChatRoom(props) {
     console.log("DEBUG: ChatRoom: messages");
     console.log(messages);
 
+    
     console.log("DEBUG: ChatRoom: messages.resolve");
     const data = Promise.resolve(messages);
     console.log(p);
