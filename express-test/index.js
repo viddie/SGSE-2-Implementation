dbac2fd511c74c78b70eb16e0ecf0099d68b91ce
Hannes Ru <viddie@hotmail.de>
2021-07-08 02:48:54 +0200

    Testing explicit DB endpoint

--- express-test/index.js
+++ express-test/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const port = 30100;
 
 var con = mysql.createConnection({
-  host: "172.17.0.9",
+  host: "172.17.0.55",
   user: "root",
   password: "passwort123!"
 });
