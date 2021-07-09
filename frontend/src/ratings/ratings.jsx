9785561da122fdc3e6c89b09f7f4bdbdd5c80ed7
tnentwig <tristan.nentwig@googlemail.com>
2021-07-09 23:06:22 +0200

    Ratings in zwei Komponenten aufgeteilt

--- frontend/src/ratings/ratings.jsx
+++ frontend/src/ratings/ratings.jsx
@@ -1,62 +1,33 @@
 import { useState, useEffect } from 'react';
 import { FaStar } from 'react-icons/fa';
+import { styles, color } from './styles'
 
-const colors = {
-    orange: '#FFBA5A',
-    grey: '#a9a9a9',
-    green: '#40E0D0'
-};
-
-const Ratings = (props) => {
+export const MakeRating = (props) => {
+    const [numRatings, setNumRating] = useState(0);
     const [currentValue, setCurrentValue] = useState(0);
     const [hoverValue, setHoverValue] = useState(undefined);
     const [error, setError] = useState(false);
     const [avgRating, setAvgRating] = useState(0);
-    const [numRatings, setNumRating] = useState(0);
+
     const stars = Array(5).fill(0);
-
-    useEffect(() => {
-        fetch(`http://sgse2.ad.fh-bielefeld.de/api/ratings/ratings/UserID`, {
-            method: 'GET',
-            headers: {
-                Authorization:
-                    'Bearer ' + sessionStorage.getItem('accessToken'),
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((res) => {
-                if (res.ok) {
-                    console.log(res.body);
-                    res.json().then((data) => {
-                        setAvgRating(Math.round(data.avgStar));
-                        setNumRating(data.totalRatings);
-                        console.log(data);
-                    });
-                } else {
-                    setError(true);
-                }
-            })
-
-            .catch(() => {});
-    }, []);
+    const userID = sessionStorage.getItem(
+        'userID'
+    );
 
     function handleClick(value) {
         fetch(
-            `http://sgse2.ad.fh-bielefeld.de/api/ratings/ratings/UserID/${sessionStorage.getItem(
-                'userID'
-            )}/Rating/${value}`,
+            `http://sgse2.ad.fh-bielefeld.de/api/ratings/ratings/UserID/${userID}/Rating/${value}`,
             {
                 method: 'POST',
                 headers: {
                     Authorization:
                         'Bearer ' + sessionStorage.getItem('accessToken'),
-                    'Content-Type': 'application/json'
+                        'Content-Type': 'application/json'
                 }
             }
         )
             .then((res) => {
                 if (res.ok) {
-                    console.log(res.body);
                     res.json().then((data) => {
                         setAvgRating(Math.round(data.avgStar));
                         setNumRating(data.totalRatings);
@@ -68,7 +39,6 @@ const Ratings = (props) => {
             })
 
             .catch(() => {});
-
         setCurrentValue(value);
     }
 
@@ -105,6 +75,40 @@ const Ratings = (props) => {
                     );
                 })}
             </div>
+            <RatingStars userID={userID}></RatingStars>
+        </div>
+    );
+};
+
+export const RatingStars = (props) => {
+    const [numRatings, setNumRating] = useState(0);
+    const [currentValue, setCurrentValue] = useState(0);
+
+    useEffect(() => {
+        fetch(`http://sgse2.ad.fh-bielefeld.de/api/ratings/ratings/UserID/`+props.userID, {
+            method: 'GET',
+            headers: {
+                Authorization:
+                    'Bearer ' + sessionStorage.getItem('accessToken'),
+                    'Content-Type': 'application/json'
+            }
+        })
+            .then((res) => {
+                if (res.ok) {
+                    console.log(res.body);
+                    res.json().then((data) => {
+                        setAvgRating(Math.round(data.avgStar));
+                        setNumRating(data.totalRatings);
+                    });
+                } else {
+                    setError(true);
+                }
+            })
+            .catch(() => {});
+    }, []);
+
+    return (
+        <div>
             <p> Anzahl Sterne: {currentValue}</p>
             <p> Durchschnittliche Bewertung</p>
             <div style={styles.stars}>
@@ -126,33 +130,6 @@ const Ratings = (props) => {
             </div>
             <p> Anzahl der Bewertungen: {numRatings}</p>
         </div>
-    );
-};
+    )
 
-const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-    },
-    stars: {
-        display: 'flex',
-        flexDirection: 'row'
-    },
-    textarea: {
-        border: '1px solid #a9a9a9',
-        borderRadius: 5,
-        padding: 10,
-        margin: '20px 0',
-        minHeight: 100,
-        width: 300
-    },
-    button: {
-        border: '1px solid #a9a9a9',
-        borderRadius: 5,
-        width: 300,
-        padding: 10
-    }
-};
-
-export default Ratings;
+}
---
+++ frontend/src/ratings/styles.js
@@ -0,0 +1,32 @@
+
+export const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    stars: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    textarea: {
+        border: '1px solid #a9a9a9',
+        borderRadius: 5,
+        padding: 10,
+        margin: '20px 0',
+        minHeight: 100,
+        width: 300
+    },
+    button: {
+        border: '1px solid #a9a9a9',
+        borderRadius: 5,
+        width: 300,
+        padding: 10
+    }
+};
+
+export const colors = {
+    orange: '#FFBA5A',
+    grey: '#a9a9a9',
+    green: '#40E0D0'
+};
