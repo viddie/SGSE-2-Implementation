0bc207573249f51a5609c2c689d4f13b71a11990
tnentwig <tristan.nentwig@googlemail.com>
2021-07-09 13:05:36 +0200

    Baby its the last fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Chat from './chat/chat';
 import Login from './user/login';
 import SignUp from './user/signUp';
 import Ratings from './ratings/ratings';
+import CreateArticle from './article/createArticle';
 import ErrorPageNotFound from './notFoundError/routeNotFound';
 import { Switch, BrowserRouter, Route, Redirect } from 'react-router-dom';
 
