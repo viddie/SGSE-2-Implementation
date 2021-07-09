ef7221e05c33cde339ccc011afe81ef0df780b3c
tnentwig <tristan.nentwig@googlemail.com>
2021-07-10 00:25:45 +0200

    Switch back to Development

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   devServer: {
     contentBase: path.join(__dirname, "build"),
     compress: true,
