0174bcae184a393f0fee4a0ba592164679436a8f
Niklas3006 <82842605+Niklas3006@users.noreply.github.com>
2021-07-10 14:19:32 +0200

    Update ratings.js

--- ratings/models/ratings.js
+++ ratings/models/ratings.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const RatingsSchema = new mongoose.Schema({
     ID: {
-        type: Number,
+        type: String,
         required: true,
     },
     avgStar: 
