a1b9a9653bc8f8b8995a278fffed1060340ed136
Marius Warning <MWarning.Minden@gmail.com>
2021-07-10 22:36:30 +0200

    style changes in chat

--- frontend/src/chat/chatSelection.jsx
+++ frontend/src/chat/chatSelection.jsx
@@ -78,7 +78,7 @@ function Entry(props) {
 
     return (
         <div className="chat_selection_entry">
-            <Link to={"/userChat-"+props.receiver} style={{display: 'inline-block'}}>
+            <Link className="chat_selection_link" to={"/userChat-"+props.receiver}>
                 <div className="chat_select_item" id="chat_select_img_container">
                     <img
                         className="chat_select_img"
