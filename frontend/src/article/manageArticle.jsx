25ae5dea87c874d7da22d82775d131bbed7a8171
tnentwig <tristan.nentwig@googlemail.com>
2021-07-10 13:34:28 +0200

    Error hinzugefügt

--- frontend/src/article/manageArticle.jsx
+++ frontend/src/article/manageArticle.jsx
@@ -5,12 +5,28 @@ import { useHistory, useParams } from 'react-router-dom';
 export const EditArticle = (props) => {
     const history = useHistory();
     let { id } = useParams();
-    const [heading, setHeading] = useState(props.article.heading);
-    const [description, setDescription] = useState(props.article.description);
-    const [price, setPrice] = useState(props.article.price);
-    const [tags, setTags] = useState(props.article.tags.join());
-    const [category, setCategory] = useState(props.article.category);
+
+    const [heading, setHeading] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState(0.0);
+    const [tags, setTags] = useState('');
+    const [category, setCategory] = useState('household');
+    const [files, setFiles] = useState([]);
     const [error, setError] = useState(false);
+
+    useEffect(() => {
+        fetch('/api/offers/article/' + id)
+            .then((res) => res.json())
+            .then((res) => {
+            setHeading(res.heading);
+            setDescription(res.price); 
+            setTags(res.tags.join(",")); 
+            setCategory(res.category); 
+            })
+            .catch(() => setErrors(true));
+    }, []);
+
+
 
     function validateForm() {
         return heading.length > 0 && description.length > 0;
@@ -50,89 +66,89 @@ export const EditArticle = (props) => {
             })
             .catch(() => {});
     }
-
     return (
-        <Container className="m-auto" style={{ maxWidth: '60%' }}>
-            {error && <Alert variant={'warning'}>Email nicht valide!</Alert>}
-            <Form onSubmit={handleSubmit}>
-                <Form.Group size="lg" controlId="heading">
-                    <Form.Label>Überschrift</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={heading}
-                        onChange={(e) => setHeading(e.target.value)}
-                    />
-                </Form.Group>
+            <Container className="m-auto" style={{ maxWidth: '60%' }}>
+                {error ?
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group size="lg" controlId="heading">
+                            <Form.Label>Überschrift</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={heading}
+                                onChange={(e) => setHeading(e.target.value)}
+                            />
+                        </Form.Group>
 
-                <Form.Group size="lg" controlId="description">
-                    <Form.Label>Beschreibung</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        rows={3}
-                        autoFocus
-                        type="text"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </Form.Group>
+                        <Form.Group size="lg" controlId="description">
+                            <Form.Label>Beschreibung</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                autoFocus
+                                type="text"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </Form.Group>
 
-                <Form.Group size="lg" controlId="price">
-                    <Form.Label>Mein Preis</Form.Label>
-                    <Form.Control
-                        type="number"
-                        placeholder="0.00"
-                        step="0.01"
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)}
-                    />
-                </Form.Group>
+                        <Form.Group size="lg" controlId="price">
+                            <Form.Label>Mein Preis</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="0.00"
+                                step="0.01"
+                                value={price}
+                                onChange={(e) => setPrice(e.target.value)}
+                            />
+                        </Form.Group>
 
-                <Form.Group size="lg" controlId="tags">
-                    <Form.Label>Tags</Form.Label>
-                    <Form.Control
-                        autoFocus
-                        type="text"
-                        value={tags}
-                        onChange={(e) => setTags(e.target.value)}
-                        aria-describedby="tagsHelper"
-                    />
-                    <Form.Text id="tagsHelper" muted>
-                        Gib hier prägnante Tags kommasepariert ein. Es wird die
-                        Erreichbarkeit erhöhen!
-                    </Form.Text>
-                </Form.Group>
+                        <Form.Group size="lg" controlId="tags">
+                            <Form.Label>Tags</Form.Label>
+                            <Form.Control
+                                autoFocus
+                                type="text"
+                                value={tags}
+                                onChange={(e) => setTags(e.target.value)}
+                                aria-describedby="tagsHelper"
+                            />
+                            <Form.Text id="tagsHelper" muted>
+                                Gib hier prägnante Tags kommasepariert ein. Es wird die
+                                Erreichbarkeit erhöhen!
+                            </Form.Text>
+                        </Form.Group>
 
-                <Form.Group>
-                    <Form.Control
-                        value={category}
-                        onChange={(e) => setCategory(e.target.value)}
-                        as="select"
-                    >
-                        <option value="household">Haushaltswaren</option>
-                        <option value="electronics">Elektroartikel</option>
-                        <option value="antiques">Antiquitäten</option>
-                        <option value="miscellaneous">Sonstiges</option>
-                    </Form.Control>
-                </Form.Group>
+                        <Form.Group>
+                            <Form.Control
+                                value={category}
+                                onChange={(e) => setCategory(e.target.value)}
+                                as="select"
+                            >
+                                <option value="household">Haushaltswaren</option>
+                                <option value="electronics">Elektroartikel</option>
+                                <option value="antiques">Antiquitäten</option>
+                                <option value="miscellaneous">Sonstiges</option>
+                            </Form.Control>
+                        </Form.Group>
 
-                <Form.Group>
-                    <Form.File
-                        id="custom-file"
-                        data-browse="Datei suchen"
-                        custom
-                        onChange={(e) => setFiles(e.target.files)}
-                    />
-                </Form.Group>
-                <Button
-                    block
-                    size="lg"
-                    onClick={() => handleSubmit()}
-                    disabled={!validateForm()}
-                >
-                    Artikel einstellen
-                </Button>
-            </Form>
-        </Container>
+                        <Form.Group>
+                            <Form.File
+                                id="custom-file"
+                                data-browse="Datei suchen"
+                                custom
+                                onChange={(e) => setFiles(e.target.files)}
+                            />
+                        </Form.Group>
+                        <Button
+                            block
+                            size="lg"
+                            onClick={() => handleSubmit()}
+                            disabled={!validateForm()}
+                        >
+                            Artikel einstellen
+                        </Button>
+                    </Form>
+                : <div>Artikel konnte nicht geladen werden!</div>}
+            </Container>
     );
 };
 
