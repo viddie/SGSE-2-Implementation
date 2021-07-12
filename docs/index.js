16b1cd8869a6578b3e9774fb409874d4964e0a78
Luis Deutsch <LuisDeutsch98@gmail.com>
2021-07-12 22:25:12 +0200

    made multiple files possible

--- docs/index.js
+++ docs/index.js
@@ -7,10 +7,15 @@ const app = express();
 // Hier die Swagger Beschreibungen einfügen
 const offers = require('./swagger/offers.json');
 const email = require("./swagger/email.json");
+
+var options = {}
+
 // Und deployen
-app.use('/', swaggerUi.serve);
-app.get('/offers', swaggerUi.setup(offers));
-app.get('/email', swaggerUi.setup(email));
+//app.use('/offers', swaggerUi.serve);
+//app.get('/offers', swaggerUi.setup(offers));
+//app.get('/email', swaggerUi.setup(email));
+app.use('/offers', swaggerUi.serveFiles(offers, options), swaggerUi.setup(offers));
+app.use('/email', swaggerUi.serveFiles(email, options), swaggerUi.setup(email));
 
 // Starte den Server
 app.listen(3000, err => {
