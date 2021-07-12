ecffac944862bb755d7b33f6b42fc2a6cb30e58e
tnentwig <tristan.nentwig@googlemail.com>
2021-07-12 18:30:18 +0200

    Json angepasst

--- offers/routes/api-docs/index.js
+++ offers/routes/api-docs/index.js
@@ -1,8 +1,16 @@
-const router = require('express').Router();
+const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('./offers.json');
 
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+const app = express();
+app.use('/', swaggerUi.serve);
+app.get('/', swaggerUi.setup(swaggerDocument));
 
-module.exports = router;
+// Starte den Server
+
+app.listen(22222, err => {
+    if (err)
+        throw err
+    console.log('Ich bin der Niklas und habe kein Leben')
+})
+
