ac437690b1b5328cc14638c6afc88f906be05f31
Luis Deutsch <LuisDeutsch98@gmail.com>
2021-07-12 22:13:52 +0200

    email microservice swagger docs

--- docs/index.js
+++ docs/index.js
@@ -6,9 +6,12 @@ const app = express();
 
 // Hier die Swagger Beschreibungen einfügen
 const offers = require('./swagger/offers.json');
+const email = require("./swagger/email.json");
 // Und deployen
 app.use('/offers', swaggerUi.serve);
 app.get('/offers', swaggerUi.setup(offers));
+app.use('/email', swaggerUi.serve);
+app.get('/email', swaggerUi.setup(email));
 
 // Starte den Server
 app.listen(3000, err => {
