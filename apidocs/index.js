16bb66eb4825da4d0bc02e9800189bde55190cf5
tnentwig <tristan.nentwig@googlemail.com>
2021-07-12 18:44:50 +0200

    Init swagger docs

---
+++ apidocs/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
+const app = express();
+app.use('/', swaggerUi.serve);
+
+// Hier die Swagger Beschreibungen einfügen
+const offers = require('./swagger/offers.json');
+// Und deployen
+app.get('/offers', swaggerUi.setup(offers));
+
+// Starte den Server
+app.listen(22222, err => {
+    if (err)
+        throw err
+    console.log('Ich bin der Niklas und habe kein Leben')
+})
+
