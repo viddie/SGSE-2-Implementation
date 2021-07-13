9a3e32ef77629843d37db8bf7f730651b7b4389e
Niklas3006 <82842605+Niklas3006@users.noreply.github.com>
2021-07-13 12:43:42 +0200

    edited style

--- ratings/routes/ratings.js
+++ ratings/routes/ratings.js
@@ -1,160 +1,151 @@
-const { json } = require("express")
-const express = require("express")
-const router = express.Router()
-const User = require('../models/ratings')
+const { json } = require("express");
+const express = require("express");
+const router = express.Router();
+const User = require("../models/ratings");
 
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
+//tokensecret zum Verschlüssen
+const accessTokenSecret = "somerandomaccesstoken";
 
-const accessTokenSecret = 'somerandomaccesstoken';
-
+//Authentifizierung des JWT-token
 const authenticateJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
 
-        jwt.verify(token, accessTokenSecret, (err, user) => {
-            if (err) {
-                return res.sendStatus(403);
-            }
+    //Entschlüsselung durch tokensecret und Weitergabe User-Informationen
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
 
-            req.validUser = user;
-            next();
-        });
-    } else {
-        res.sendStatus(401);
-    }
-}
+      //return user als req-Parameter
+      req.validUser = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
 
-// Getting all
-router.get('/', authenticateJWT, async (req,res) =>
-{
-    try{
-        const users = await User.find()
-        res.json(users)
-    }catch(err)
-    {
-        res.status(500).json({message: err.message})
-    }
-})
+//Überprüfungsschnittstelle Ausagbae aller User
+router.get("/", authenticateJWT, async (req, res) => {
+  try {
+    const users = await User.find();
 
-// Getting one
-router.get('/UserID/:id', userTokenAcces, (req,res) =>
-{
-    res.json(res.tokenuser)
-})
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-router.post("/CreateUser",authenticateJWT, async (req,res) =>
-{
-    const user = new User({
-        ID: req.validUser.id 
-    })
+//Abfrage einer Userbewertung über userid
+router.get("/UserID/:id", getUser, (req, res) => {
+  res.json(res.tokenuser);
+});
 
-    try{
-        const newUser = await user.save()
-        res.status(201).json(newUser)
-    }catch(err){
-        res.status(400).json({message: err.message})
-    }
-})
-
-router.post('/UserID/:id/Rating/:star_num',authenticateJWT, getUser, async (req,res) =>
-{
+//Anlegen oder updaten einer Userbewertung
+router.post(
+  "/UserID/:id/Rating/:star_num",
+  authenticateJWT,
+  getUser,
+  async (req, res) => {
+    //Funktion für die Berechnung der durschnittlichen Userbewertung
     let average = (array) => array.reduce((a, b) => a + b) / array.length;
 
-    try{
-        try{
-        doubleRating = await User.findOne({ID: req.params.id, 
-            RaterIDs: req.validUser.id})
-        console.log(doubleRating)}
-        catch(err){
-            res.status(400).json({message: err.message});
+    try {
+      try {
+        //Untersuchen, ob User schon vorhanden
+        doubleRating = await User.findOne({
+          ID: req.params.id,
+          RaterIDs: req.validUser.id,
+        });
+      } catch (err) {
+        res.status(400).json({ message: err.message });
+      }
+      //Sofern die Bewertung schon aufgetreten
+      if (doubleRating != null) {
+
+        //Zuordnung BewertungsID und Bewertung über die Arrays und ändern der Bewertung
+        index = res.user.RaterIDs.indexOf(req.validUser.id);
+        tmp = res.user.MappedRatings;
+        tmp[index] = Number(req.params.star_num);
+
+        //Anlegen eines neuen Eintrages 
+        const newUser = new User({
+          ID: doubleRating.ID,
+          RaterIDs: doubleRating.RaterIDs,
+          MappedRatings: tmp,
+          avgStar: average(tmp),
+          totalRatings: doubleRating.totalRatings,
+        });
+
+        //Neuer Eintrag und alten löschen
+        updatedUser = await newUser.save();
+        await doubleRating.remove();
+
+        //Rückgabe aktualiserte Bewertung
+        res.json(updatedUser);
+      } else {
+        try {
+
+          //Aktualisieren der Bewertungsparameter
+          res.user.RaterIDs.push(req.validUser.id);
+          res.user.MappedRatings.push(req.params.star_num);
+          res.user.avgStar = average(res.user.MappedRatings);
+          res.user.totalRatings = res.user.totalRatings + 1;
+
+          //Aktualisierte Bewertung speichern
+          const updatedUser = await res.user.save();
+
+           //Rückgabe aktualiserte Bewertung
+          res.json(updatedUser);
+        } catch (err) {
+          res.status(400).json({ message: err.message });
         }
-        if(doubleRating != null){
-            index = res.user.RaterIDs.indexOf(req.validUser.id);
-            tmp = res.user.MappedRatings;
-            tmp[index] = Number(req.params.star_num);
-            
-            const newUser = new User({
-                ID: doubleRating.ID,
-                RaterIDs: doubleRating.RaterIDs,
-                MappedRatings: tmp,
-                avgStar: average(tmp),
-                totalRatings: doubleRating.totalRatings
-            })
-            //res.user.avgStar = Number(req.params.star_num);
-
-            updatedUser = await newUser.save()  
-            await doubleRating.remove()    
-          
-            res.json(updatedUser)
-        }else{
-            try{
-                res.user.RaterIDs.push(req.validUser.id);
-                res.user.MappedRatings.push(req.params.star_num);
-                res.user.avgStar = average(res.user.MappedRatings)
-                res.user.totalRatings = res.user.totalRatings + 1
-                const updatedUser = await res.user.save()              
-                res.json(updatedUser)
-            }catch (err){
-                res.status(400).json({message: err.message})
-                }
-        }
-    }catch(err){
-        return res.status(500).json({message: err.message})
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
     }
-})
+  }
+);
 
-router.delete('/:id',authenticateJWT, getUser, async (req,res) =>
-{
-    try{
-        await res.user.remove()
-        res.json({message: 'Deleted User'})
-    }catch (err)
-    {
-        res.status(500).json({message: err.message})
+//Löschen eines Bewertungseintrages
+router.delete("/:id", authenticateJWT, getUser, async (req, res) => {
+  try {
+    await res.user.remove();
+    res.json({ message: "Deleted User" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//Finden eines Bewertungseintrages oder erstellen eines nicht vorhandenen
+async function getUser(req, res, next) {
+  try {
+    user = await User.findOne({ ID: req.params.id });
+
+    //falls Benutzer nicht gefunden werden kann
+    if (user == null) {
+      //Erstellen eines neunen Nutzers
+      const newUser = new User({
+        ID: req.params.id,
+      });
+      try {
+        //Speichern eines neuen Bewertungseintrages
+        user = await newUser.save();
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
     }
-})
-
-
-async function getUser(req, res, next){
-    try{
-        user = await User.findOne({ID: req.params.id})
-        
-        if(user == null){
-            const newUser = new User({
-                ID: req.params.id,
-            })
-            try{
-                user = await newUser.save()
-            }catch(err){
-                return res.status(400).json({message: err.message})
-            }
-        }
-    }catch (err)
-    {
-        return res.status(500).json({message: err.message})
-    }
-    res.user = user
-    next()
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.user = user;
+  next();
 }
 
-async function userTokenAcces(req, res, next){
-    try{
-        user = await User.findOne({ID: req.params.id})
-
-        if(user == null){
-            return res.status(404).json({message: 'Invalid User'})
-        }
-    }catch (err)
-    {
-        return res.status(500).json({message: err.message})
-    }
-    res.tokenuser = user
-    next()
-}
-
-
-module.exports = router
+module.exports = router;
