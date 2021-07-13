be1e7226a55bafd8399b077b88e49a5a207242f2
Niklas3006 <82842605+Niklas3006@users.noreply.github.com>
2021-07-13 10:20:16 +0200

    important feature added

--- docs/index.js
+++ docs/index.js
@@ -7,17 +7,21 @@ const app = express();
 // Hier die Swagger Beschreibungen einfügen
 const offers = require('./swagger/offers.json');
 const email = require("./swagger/email.json");
+const email = require("./swagger/ratings.json");
 
 var options = {}
 
 // Und deployen
 app.use('/docs-offers', swaggerUi.serveFiles(offers, options), swaggerUi.setup(offers));
 app.use('/docs-email', swaggerUi.serveFiles(email, options), swaggerUi.setup(email));
+app.use('/docs-ratings', swaggerUi.serveFiles(ratings, options), swaggerUi.setup(ratings));
 
 // Starte den Server
 app.listen(3000, err => {
     if (err)
         throw err
     console.log('Ich bin der Niklas und habe kein Leben')
+    console.log('Tristan: "Leider bin ich meinen jungen Jahren schon so ausgebrannt, dass ich einen Hörsturz habe"')
+
 })
 
